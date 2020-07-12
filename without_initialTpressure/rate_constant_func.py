60a0f3ca82ae9a5afab16aab547659dac183b28c
Psalm116 <58156737+domoina27@users.noreply.github.com>
2020-07-12 16:22:01 -0500

    Add files via upload

---
+++ without_initialTpressure/rate_constant_func.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+def calc_k_arr(T, nu, Eact):
+    """
+    Calculate reaction rate constant for a surface reaction
+    
+    T       - Temperature in K
+    nu      - Pre-exponential factor in s^-1
+    Eact    - Activation energy in J/mol
+    """
+    R = 8.3144598 # gas constant
+    return nu * np.exp(-Eact / (R * T))
+
+def calc_kads(T, P, A, m):
+    """
+    Reaction rate constant for adsorption
+    
+    T           - Temperature in K
+    P           - Pressure in Pa
+    A           - Surface area in m^2
+    m           - Mass of reactant in kg
+    """
+    kb = 1.38064852E-23 # boltzmann constant
+    return P*A / np.sqrt(2 * np.pi * m * kb * T)
+
+def calc_kdes(T, A, m, sigma, theta_rot, Edes):
+    """
+    Reaction rate constant for desorption
+    
+    T           - Temperature in K
+    A           - Surface area in m^2
+    m           - Mass of reactant in kg
+    sigma       - Symmetry number
+    theta_rot   - Rotational temperature in K
+    Edes        - Desorption energy in J/mol
+    """
+    kb = 1.38064852e-23 # boltzmann constant
+    h = 6.62607004e-34  # planck constant
+    R = 8.3144598       # gas constant
+    return kb * T**3 / h**3 * A * (2 * np.pi * m * kb) / \
+        (sigma * theta_rot) * np.exp(-Edes / (R*T))
+
+# Derivative of Rate constants in relation to Temperature
+#--------------------------------------------------------
+def dksur_dT(T, nu, Eact):
+    """
+    Calculate reaction rate constant for a surface reaction
+    
+    T       - Temperature in K
+    nu      - Pre-exponential factor in s^-1
+    Eact    - Activation energy in J/mol
+    """
+    R = 8.3144598 # gas constant
+    return nu *Eact/(R*T**2)* np.exp(-Eact / (R * T))    
+
+
+def dkdes_dT(T, A, m, sigma, theta_rot, Edes):
+    """
+    Reaction rate constant for desorption
+    
+    T           - Temperature in K
+    A           - Surface area in m^2
+    m           - Mass of reactant in kg
+    sigma       - Symmetry number
+    theta_rot   - Rotational temperature in K
+    Edes        - Desorption energy in J/mol
+    """
+    kb = 1.38064852e-23 # boltzmann constant
+    h = 6.62607004e-34  # planck constant
+    R = 8.3144598       # gas constant    
+    
+    K = kb/ h**3 * A * (2 * np.pi * m * kb) / (sigma * theta_rot)
+    return K/R*T* np.exp(-Edes / (R*T))* (Edes+3*R*T)
